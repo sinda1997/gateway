6e4bbc74fea4866a10b841ae4292e7aba543c40e
Sinda25051997 <69511737+Sinda25051997@users.noreply.github.com>
2022-06-08 10:20:20 +0100

    first

---
+++ src/main/java/tn/com/ebanking/EbankingGatewayApplication.java
@@ -0,0 +1,22 @@
+package tn.com.ebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient 
+public class EbankingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingGatewayApplication.class, args);
+	}@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+
+}
---
+++ src/test/java/tn/com/ebanking/BfiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.com.ebanking;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
